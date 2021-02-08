f09bddf5eec721b25eaae6c47b1a0e9dd45a4417
Liam Wohlfart <liam.wohlfart0@gmail.com>
2021-02-08 11:58:32 +0100

    Add Thing.cs and basic example of Bindings

--- Logic/Logic.Ui/MainViewModel.cs
+++ Logic/Logic.Ui/MainViewModel.cs
@@ -1,9 +1,28 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace lmwohlfart.tsbw.WpfMvvm.Logic.Ui.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        public string WindowTitle { get; private set; }
+
+        private string input;
+
+        public string Input
+        {
+            get => input;
+            set
+            {
+                input = value;
+                RaisePropertyChanged("Input");
+            }
+        }
+
+        public ObservableCollection<Thing> Things { get; set; }
+
         public MainViewModel()
         {
             if (IsInDesignMode)
@@ -12,10 +31,31 @@ namespace lmwohlfart.tsbw.WpfMvvm.Logic.Ui.ViewModel
             }
             else
             {
+                Things = new ObservableCollection<Thing>();
                 WindowTitle = "MainWindow";
+                Input = "Hallo Welt";
             }
         }
 
-        public string WindowTitle { get; private set; }
+        private ICommand _onButtonClick;
+
+        public ICommand OnButtonClick
+        {
+            get
+            {
+                if (_onButtonClick == null)
+                {
+                    _onButtonClick = new RelayCommand(() =>
+                    {
+                        if (!string.IsNullOrWhiteSpace(Input))
+                        {
+                            Things.Add(new Thing(Input));
+                        }
+                    });
+                }
+
+                return _onButtonClick;
+            }
+        }
     }
 }
---
+++ Logic/Logic.Ui/Thing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lmwohlfart.tsbw.WpfMvvm.Logic.Ui
+{
+    public class Thing
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Thing(string name) => this.name = name;
+    }
+}
